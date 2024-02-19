ae093466ad2ef8b9c2139b0cbb3c4bd0da51110b
kevin <kevin@kevin.kevin.com>
2024-02-19 16:40:42 +0300

    class creation

---
+++ OOP/class.py
@@ -0,0 +1,13 @@
+#creating a class in python
+class Pen:
+    color1 = "Blue"
+    
+    #creating a method
+    def write(self):
+        print("I am writting")
+        
+#creating an object/an instance of a class
+first_pen = Pen()
+
+first_pen.write() #calling the method
+    
